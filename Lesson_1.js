4afcd72c705de5bf3ec281b6c8a55dd77fee363f
Olga Lukianova <103361761+Olga-Igorevna@users.noreply.github.com>
2022-05-12 14:17:08 +0300

    Add files via upload

---
+++ Lesson_1.js
@@ -0,0 +1,389 @@
+// let yolochka4 = 4
+// let yolochka6 = 6
+// let yolochka_name = "Zelenaya"
+// let result_2 = yolochka6 + yolochka_name
+
+// let result = yolochka4 + yolochka6
+
+// console.log("result - " + yolochka4 + yolochka6)
+
+// console.log(result)
+// console.log(result_2)
+
+// let t = true
+// let f = false
+
+// console.log(t+f)
+
+
+
+// if (true) {
+//     let apples = 3+5
+//     console.log("if true = " + apples)
+// } else {
+//     let meat = 2+2
+//     console.log("if not true = " + meat)
+// }
+
+// if (t) {
+//     let apples = 3+5
+//     console.log("if true = " + apples)
+// } else {
+//     let meat = 2+2
+//     console.log("if not true = " + meat)
+// }
+
+
+
+// if (f) {
+//     let apples = 3+5
+//     console.log("if true = " + apples)
+//  }
+//   else if (t) {
+//     let milk = 2
+//     console.log("else if = " + milk)
+// } else {
+//     let meat = 2+2
+//     console.log("if not true = " + meat)
+// }
+
+
+
+// if (yolochka4 + yolochka6 == 10) {
+//     let apples = 3+5
+//     console.log("if true = " + apples)
+//  }
+//   else if (f) {
+//     let milk = 2
+//     console.log("else if = " + milk)
+// } else {
+//     let meat = 2+2
+//     console.log("if not true = " + meat)
+// }
+
+
+
+// if (yolochka4 + yolochka6 == 11) {
+//     let apples = 3+5
+//     console.log("if true = " + apples)
+//  }
+//   else if (f) {
+//     let milk = 2
+//     console.log("else if = " + milk)
+// } else {
+//     let meat = 2+2
+//     console.log("if not true = " + meat)
+// }
+
+
+
+// if (yolochka4 + yolochka6 == 10) {
+//     let apples = 3+5
+//     console.log("if true = " + apples)
+//  }
+//   else if (t) {
+//     let milk = 2
+//     console.log("else if = " + milk)
+// } else {
+//     let meat = 2+2
+//     console.log("if not true = " + meat)
+// }
+
+
+
+// if (yolochka4 + yolochka6 == 11) {
+//     let apples = 3+5
+//     console.log("if true = " + apples)
+//  }
+//   else if (yolochka4 + yolochka6 == 12) {
+//     let milk = 2
+//     console.log("else if = " + milk)
+// } else {
+//     let meat = 2+2
+//     console.log("if not true = " + meat)
+// }
+
+
+
+// if (yolochka4 + yolochka6 == 11) {
+//     let apples = 3+5
+//     console.log("if true = " + apples)
+//  }
+//   else if (yolochka4 + yolochka6 == 12) {
+//     let milk = 2
+//     console.log("else if = " + milk)
+// }
+// else if (yolochka4 + yolochka6 == 10) {
+//   let water = 15
+//   console.log("else if 2 = " + water)
+// } else {
+//     let meat = 2+2
+//     console.log("if not true = " + meat)
+// }
+
+
+
+// if (yolochka4 + yolochka6 == "10") {
+//     let apples = 3+5
+//     console.log("if true = " + apples)
+//  }
+//   else if (yolochka4 + yolochka6 == 12) {
+//     let milk = 2
+//     console.log("else if = " + milk)
+// }
+// else if (yolochka4 + yolochka6 == 10) {
+//   let water = 15
+//   console.log("else if 2 = " + water)
+// } else {
+//     let meat = 2+2
+//     console.log("if not true = " + meat)
+// }
+
+
+
+// if (yolochka4 + yolochka6 === "10") {
+//     let apples = 3+5
+//     console.log("if true = " + apples)
+//  }
+//   else if (yolochka4 + yolochka6 == 12) {
+//     let milk = 2
+//     console.log("else if = " + milk)
+// }
+// else if (yolochka4 + yolochka6 == 10) {
+//   let water = 15
+//   console.log("else if 2 = " + water)
+// } else {
+//     let meat = 2+2
+//     console.log("if not true = " + meat)
+// }
+
+
+
+// let age = 18
+
+// if (age >= 18 && age <60) {
+    
+//     console.log("Full content ")
+//  }
+//   else if (age < 18) {
+    
+//     console.log("Multiki ")
+// }
+// else if ( age >= 60 ) {
+  
+//   console.log("Kultura")
+// } else {
+    
+//     console.log("Good Luck!")
+// }
+
+
+
+// let age = 17
+
+// if (age >= 18 && age <60) {
+    
+//     console.log("Full content ")
+//  }
+//   else if (age < 18) {
+    
+//     console.log("Multiki ")
+// }
+// else if ( age >= 60 ) {
+  
+//   console.log("Kultura")
+// } else {
+    
+//     console.log("Good Luck!")
+// }
+
+
+
+// let age = 70
+
+// if (age >= 18 && age <60) {
+    
+//     console.log("Full content ")
+//  }
+//   else if (age < 18) {
+    
+//     console.log("Multiki ")
+// }
+// else if ( age >= 60 ) {
+  
+//   console.log("Kultura")
+// } else {
+    
+//     console.log("Good Luck!")
+// }
+
+
+
+// let age = "Nexta"
+
+// if (age >= 18 && age <60) {
+    
+//     console.log("Full content ")
+//  }
+//   else if (age < 18) {
+    
+//     console.log("Multiki ")
+// }
+// else if ( age >= 60 ) {
+  
+//   console.log("Kultura")
+// } else {
+    
+//     console.log("Good Luck!")
+// }
+
+
+
+// let age = ""
+
+// if (age >= 18 && age <60) {
+    
+//     console.log("Full content ")
+//  }
+//   else if (age < 18) {
+    
+//     console.log("Multiki ")
+// }
+// else if ( age >= 60 ) {
+  
+//   console.log("Kultura")
+// } else {
+    
+//     console.log("Good Luck!")
+// }
+
+
+
+// let age = null
+
+// if (age >= 18 && age <60) {
+    
+//     console.log("Full content ")
+//  }
+//   else if (age < 18) {
+    
+//     console.log("Multiki ")
+// }
+// else if ( age >= 60 ) {
+  
+//   console.log("Kultura")
+// } else {
+    
+//     console.log("Good Luck!")
+// }
+
+
+
+// let age = true
+
+// if (age >= 18 && age <60) {
+    
+//     console.log("Full content ")
+//  }
+//   else if (age < 18) {
+    
+//     console.log("Multiki ")
+// }
+// else if ( age >= 60 ) {
+  
+//   console.log("Kultura")
+// } else {
+    
+//     console.log("Good Luck!")
+// }
+
+
+
+// let age = false
+
+// if (age >= 18 && age <60) {
+    
+//     console.log("Full content ")
+//  }
+//   else if (age < 18) {
+    
+//     console.log("Multiki ")
+// }
+// else if ( age >= 60 ) {
+  
+//   console.log("Kultura")
+// } else {
+    
+//     console.log("Good Luck!")
+// }
+
+
+
+// let age = 18
+
+// if (typeof(age) == "number") {
+
+//     if (age >= 18 && age <60) {
+    
+//     console.log("Full content ")
+// }
+// else if (age < 18) {
+    
+//     console.log("Multiki ")
+// }
+// else if ( age >= 60 ) {
+  
+//     console.log("Kultura")
+// } 
+// } else {
+    
+//     console.log("Good Luck!")
+// }
+
+
+
+// let age = 2
+
+// let tt = typeof(age)
+
+// console.log(tt)
+
+
+// let age = "2"
+
+// let tt = typeof(age)
+
+// console.log(tt)
+
+
+// let age = 2
+
+// let tt = typeof(typeof(age))
+
+// console.log(tt)
+
+
+// let age = 2
+// let age2 = 12
+// let tt = typeof(age)
+// let nn = typeof(age2)
+// console.log(tt)
+
+// if (tt == nn) {
+
+//     if (age >= 18 && age <60) {
+    
+//     console.log("Full content ")
+// }
+// else if (age < 18) {
+    
+//     console.log("Multiki ")
+// }
+// else if ( age >= 60 ) {
+  
+//     console.log("Kultura")
+// } 
+// } else {
+    
+//     console.log("Good Luck!")
+// }
